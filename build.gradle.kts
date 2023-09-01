f8e5ba5202569142944c7ba94aed66209b76189e
Distractic <distractic@outlook.fr>
2023-09-01 06:58:03 +0200

    feat: Add simple example

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+
+plugins {
+    embeddedKotlin("jvm")
+    embeddedKotlin("plugin.serialization")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+
+    maven("https://jitpack.io")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    val paperVersion = "1.19-R0.1-SNAPSHOT"
+    val rushyApiVersion = "2.0.0"
+    val commandApiVersion = "9.0.3"
+
+    compileOnly(kotlin("stdlib"))
+
+    "io.papermc.paper:paper-api:$paperVersion".let {
+        compileOnly(it)
+        testImplementation(it)
+    }
+
+    compileOnly("com.github.Rushyverse:api:$rushyApiVersion")
+
+    // CommandAPI framework
+    compileOnly("dev.jorel:commandapi-bukkit-core:$commandApiVersion")
+    compileOnly("dev.jorel:commandapi-bukkit-kotlin:$commandApiVersion")
+
+    testImplementation(kotlin("test"))
+}
+
+val javaVersion get() = JavaVersion.VERSION_17
+val javaVersionString get() = javaVersion.toString()
+val javaVersionInt get() = javaVersionString.toInt()
+
+kotlin {
+    jvmToolchain(javaVersionInt)
+
+    sourceSets {
+        all {
+            languageSettings {
+                optIn("kotlin.RequiresOptIn")
+                optIn("kotlin.ExperimentalStdlibApi")
+                optIn("kotlin.contracts.ExperimentalContracts")
+                optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
+            }
+        }
+    }
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = javaVersionString
+    }
+
+    withType<JavaCompile> {
+        sourceCompatibility = javaVersionString
+        targetCompatibility = javaVersionString
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+    }
+}
---
+++ src/main/kotlin/com/github/rushyverse/template/TemplatePlugin.kt
@@ -0,0 +1,44 @@
+package com.github.rushyverse.template
+
+import com.github.rushyverse.api.Plugin
+import com.github.rushyverse.api.player.Client
+import com.github.rushyverse.api.translation.ResourceBundleTranslator
+import com.github.rushyverse.api.translation.registerResourceBundleForSupportedLocales
+import com.github.shynixn.mccoroutine.bukkit.scope
+import dev.jorel.commandapi.kotlindsl.commandTree
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.job
+import kotlinx.coroutines.plus
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.entity.Player
+import java.util.*
+
+class TemplatePlugin : Plugin("idTemplate", "bundleTemplate") {
+
+    override suspend fun onEnableAsync() {
+        super.onEnableAsync()
+        modulePlugin<TemplatePlugin>()
+
+        commandTree("template") {
+            playerExecutor { player, commandArguments ->
+                player.sendMessage(Component.text("Hello, world!", NamedTextColor.GREEN))
+            }
+        }
+
+    }
+
+    override fun createTranslator(): ResourceBundleTranslator {
+        return super.createTranslator().apply {
+            registerResourceBundleForSupportedLocales(bundle, ResourceBundle::getBundle)
+        }
+    }
+
+    override fun createClient(player: Player): Client {
+        return Client(
+            player.uniqueId,
+            scope + SupervisorJob(scope.coroutineContext.job)
+        )
+    }
+}
